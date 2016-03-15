c6e51ad48e32e0f928ecd5cbae270c38fdeaadea
Horatiu <ilie.horatiu@yahoo.com>
2016-03-15 10:59:54 +0000

    Added Sound Playing

--- www/js/app.js
+++ www/js/app.js
@@ -20,20 +20,3 @@ angular.module('app', ['ionic', 'ngCordova', 'app.controllers', 'app.routes', 'a
     }
   });
 })
-
-/*example.controller("ExampleController", function($scope, $cordovaMedia, $ionicLoading) {
- 
-    $scope.play = function(src) {
-        var media = new Media(src, null, null, mediaStatusCallback);
-        $cordovaMedia.play(media);
-    }
- 
-    var mediaStatusCallback = function(status) {
-        if(status == 1) {
-            $ionicLoading.show({template: 'Loading...'});
-        } else {
-            $ionicLoading.hide();
-        }
-    }
- 
-});*/
--- www/js/sounds.js
+++ www/js/sounds.js
@@ -1,12 +1,87 @@
-function playAudio(url) {
+
+function playAudio(url){
+    if(device.platform.toLowerCase() === "android") 
+        url = "/android_asset/www/" + url;
     // Play the audio file at url
     var my_media = new Media(url,
         // success callback
         function() {},
         // error callback
-        function(err) {}
+        function(err) {navigator.notification.alert(err);}
     );
 
     // Play audio
     my_media.play();
+}
+
+function play(url, btn1, btn2) {
+    document.getElementById("ex").style.visibility = "visible";
+
+    document.getElementById('ex1').innerText=btn1;
+    document.getElementById("ex1").style.visibility = "visible";
+
+    document.getElementById('ex2').innerText=btn2;
+    document.getElementById("ex2").style.visibility = "visible";
+
+    playAudio(url);
+}
+
+
+var timeout;
+var recording = false;
+var RECORD = null;
+var ACTIVE_RECORD = null;
+
+function startRecording(url){
+
+    if(device.platform.toLowerCase() === "android") 
+        url += ".amr" ;
+    else url += ".wav";
+
+    // Play the audio file at url
+    RECORD = new Media(url,
+        // success callback
+        function() {},
+        // error callback
+        function(err) {navigator.notification.alert(err);}
+    );
+   
+    ACTIVE_RECORD = null;
+    // Record audio
+    RECORD.startRecord();
+
+}
+
+function stopRecording(){
+    RECORD.stopRecord();
+}
+
+function stopPlayingRecording(){
+    if(ACTIVE_RECORD !== null)
+        ACTIVE_RECORD.stop();
+}
+
+function pausePlayingRecording(){
+    if(ACTIVE_RECORD !== null)
+        ACTIVE_RECORD.pause();
+}
+
+function playRecording(url){
+    if(device.platform.toLowerCase() === "android")
+        url += ".amr";
+    else 
+        url += ".wav";
+
+    if(ACTIVE_RECORD === null){
+        
+        ACTIVE_RECORD = new Media(url,
+        // success callback
+        function() {},
+        // error callback
+        function(err) {navigator.notification.alert(err);}
+        );
+    }
+
+    // Play audio
+    ACTIVE_RECORD.play();
 }
